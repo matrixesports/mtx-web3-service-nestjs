edaadeb3ad2269465a5e96af71c2d6c280fa585a
rayquaza7 <sarhaangulati737@gmail.com>
2022-08-08 13:48:00 -0700

    admin endpoints done

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,7 +1,60 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('admin')
 export class AdminController {
   @Get('check/:creatorId')
-  async check(@Param('creatorId') creatorId: number) {}
+  async check(@Param('creatorId') creatorId: number) {
+    //check if creatorId exists and return address if it does
+    return {
+      exists: true,
+      address: '0x1234567890123456789012345678901234567890',
+    };
+  }
+
+  @Get('seasonId/:creatorId')
+  async seasonId(@Param('creatorId') creatorId: number) {
+    //check if creatorId exists and return address if it does
+    return {
+      seasonId: 1,
+    };
+  }
+
+  @Post('deploy')
+  async deploy(@Body('creatorId') creatorId: number) {
+    return {
+      success: true,
+    };
+  }
+
+  @Post('giveXp')
+  async giveXp(
+    @Body('creatorId') creatorId: number,
+    @Body('userAddress') userAddress: string,
+    @Body('xp') xp: number,
+  ) {
+    return {
+      success: true,
+    };
+  }
+
+  @Post('newLootbox')
+  async newLootbox(
+    @Body('creatorId') creatorId: number,
+    @Body('bundleInfo') ids: any,
+  ) {
+    return {
+      success: true,
+      lootboxId: 1001,
+    };
+  }
+
+  @Post('newSeason')
+  async newSeason(
+    @Body('creatorId') creatorId: number,
+    @Body('levelInfo') levelInfo: any,
+  ) {
+    return {
+      success: true,
+    };
+  }
 }
