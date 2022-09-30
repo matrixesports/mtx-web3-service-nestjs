effc8daebba71d82a29da0be3b7abae3e8f62663
seanbaraka <seanbaraka@gmail.com>
2022-09-30 13:29:48 +0300

    feat: registered the twitch tcp service

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { BattlePassModule } from 'src/battlepass/battlepass.module';
 import { CraftingModule } from 'src/crafting/crafting.module';
 import { MetadataModule } from 'src/metadata/metadata.module';
@@ -6,6 +8,24 @@ import { AdminController } from './admin.controller';
 
 @Module({
   controllers: [AdminController],
-  imports: [CraftingModule, BattlePassModule, MetadataModule],
+  imports: [
+    CraftingModule,
+    BattlePassModule,
+    MetadataModule,
+    ClientsModule.registerAsync([
+      {
+        name: 'TWITCH_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: config.get<string>('microservice.twitch.host'),
+            port: config.get<number>('microservice.twitch.port'),
+          },
+        }),
+      },
+    ]),
+  ],
 })
 export class AdminModule {}
