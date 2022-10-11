f410b6a1fbb118d323630a2471ae4f81b75237a8
BoManev <bomanev00@gmail.com>
2022-10-11 18:36:47 -0400

    swagger spec

--- src/api/api.dto.ts
+++ src/api/api.dto.ts
@@ -1,65 +1,95 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Requirements } from 'src/graphql.schema';
 import { LootdropRS } from 'src/reward/reward.entity';
 
 abstract class MintToken {
+  @ApiProperty({ type: String })
   userAddress: string;
+  @ApiProperty({ type: Number })
   creatorId: number;
 }
 
 abstract class IBattlePass {
+  @ApiProperty({ type: Number })
   creatorId: number;
 }
 
 export class MintXpDto implements MintToken {
+  @ApiProperty({ type: String })
   userAddress: string;
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty({ type: Number })
   amount: number;
 }
 
 export class MintTokenDto implements MintToken {
+  @ApiProperty({ type: String })
   userAddress: string;
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty({ type: Number })
   amount: number;
+  @ApiProperty({ type: Number })
   id: number;
 }
 
 export const REPUTATION_TOKEN_ID = 1000;
 export class MintRepDto implements MintToken {
+  @ApiProperty({ type: String })
   userAddress: string;
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty({ type: Number })
   amount: number;
 }
 
 export class MintPremPassDto implements MintToken {
+  @ApiProperty({ type: String })
   userAddress: string;
+  @ApiProperty({ type: Number })
   creatorId: number;
 }
 
 export class CreateLootboxDto implements IBattlePass {
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty()
   lootboxInfo: any;
 }
 
 export class CreateRecipeDto implements IBattlePass {
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty()
   inputIngredients: any;
+  @ApiProperty()
   outputIngredients: any;
 }
 
 export class CreateSeasonDto implements IBattlePass {
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty()
   levelDetails: any;
 }
 
 export class CreateLootdropDto implements LootdropRS, IBattlePass {
+  @ApiProperty({ type: Number })
   creatorId: number;
+  @ApiProperty({ type: Number })
   rewardId: number;
+  @ApiProperty({ enum: Requirements })
   requirements: Requirements;
+  @ApiProperty({ type: Number })
   threshold: number;
+  @ApiProperty({ type: String })
   start: string;
+  @ApiProperty({ type: String })
   end: string;
 }
 
 export class ShortUrl {
+  @ApiProperty({ type: String })
   shortUrl: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ErrorInterceptor } from './common/error.interceptor';
 import { GlobalFilter } from './common/filters';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -10,6 +12,20 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalFilter());
   app.useGlobalInterceptors(new ErrorInterceptor());
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('MTX Web3 Serive API')
+    .setDescription('MTX Web3 Serive API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  fs.writeFile('api.json', JSON.stringify(document), function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
