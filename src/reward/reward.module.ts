47210a097877f14386e7d484a3a854883bf71a63
seanbaraka <seanbaraka@gmail.com>
2022-11-03 14:05:36 +0300

    register twitch service client module

--- src/reward/reward.module.ts
+++ src/reward/reward.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { BattlePassModule } from 'src/battlepass/battlepass.module';
 import { InventoryModule } from 'src/inventory/inventory.module';
 import { LeaderboardModule } from 'src/leaderboard/leaderboard.module';
@@ -7,7 +9,25 @@ import { RewardService } from './reward.service';
 
 @Module({
   providers: [LootboxResolver, LootdropResolver, RewardService],
-  imports: [BattlePassModule, LeaderboardModule, InventoryModule],
+  imports: [
+    BattlePassModule,
+    LeaderboardModule,
+    InventoryModule,
+    ClientsModule.registerAsync([
+      {
+        name: 'TWITCH_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          options: {
+            host: config.get<string>('microservice.twitch.host'),
+            port: config.get<number>('microservice.twitch.port'),
+          },
+          transport: Transport.TCP,
+        }),
+      },
+    ]),
+  ],
   exports: [RewardService],
 })
 export class RewardModule {}
