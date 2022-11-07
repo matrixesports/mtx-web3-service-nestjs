6dd016e934b55195e3ef57d8b1d6b077cbe8283a
BoManev <bomanev00@gmail.com>
2022-11-06 22:46:49 -0500

    register resolver

--- src/reward/reward.module.ts
+++ src/reward/reward.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BattlePassModule } from 'src/battlepass/battlepass.module';
-import { LootboxResolver } from 'src/battlepass/battlepass.resolver';
 import { ChainModule } from 'src/chain/chain.module';
 import { InventoryModule } from 'src/inventory/inventory.module';
 import { MicroserviceModule } from 'src/microservice/microservice.module';
+import { LootdropResolver } from './reward.resolver';
 import { RewardService } from './reward.service';
 
 @Module({
-  providers: [RewardService, LootboxResolver],
+  providers: [RewardService, LootdropResolver],
   imports: [BattlePassModule, InventoryModule, ChainModule, MicroserviceModule],
   exports: [RewardService],
 })
