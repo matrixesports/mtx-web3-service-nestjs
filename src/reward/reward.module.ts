181e5514efd99e4d275ec3dd1083adce55615977
seanbaraka <seanbaraka@gmail.com>
2023-01-19 13:26:22 +0300

    feat: enable manacube api triggers for lootdrops

--- src/reward/reward.module.ts
+++ src/reward/reward.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { BattlePassModule } from 'src/battlepass/battlepass.module';
 import { ChainModule } from 'src/chain/chain.module';
 import { InventoryModule } from 'src/inventory/inventory.module';
+import { ManacubeModule } from 'src/manacube/manacube.module';
 import { MicroserviceModule } from 'src/microservice/microservice.module';
 import { LootdropResolver } from './reward.resolver';
 import { RewardService } from './reward.service';
 
 @Module({
   providers: [RewardService, LootdropResolver],
-  imports: [BattlePassModule, InventoryModule, ChainModule, MicroserviceModule],
+  imports: [BattlePassModule, InventoryModule, ChainModule, MicroserviceModule, ManacubeModule],
   exports: [RewardService],
 })
 export class RewardModule {}
